b1a903ed1ea5f223dcb12a1e223ca967d1284994
Durgesh Pandey <51474798+DurgeshpdPandey@users.noreply.github.com>
2021-02-04 10:33:09 +0530

    Update QueryConstants.java

--- QueryConstants.java
+++ QueryConstants.java
@@ -1,6 +1,6 @@
-package com.nacre.demo.util;
+package com.util;
 
 public class QueryConstants {
 	public final static String REG_INSERT_QUERY="INSERT INTO REG VALUES(?,?,?,?,?,?)";
-	public final static String LOGIN_SELECT_QUERY="SELECT * FROM LOGIN WHERE UNAME=? AND PASS=?";
+	
 }
