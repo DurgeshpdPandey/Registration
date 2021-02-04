98870495c4d45528bb73612abee0faea40fa9e45
Durgesh Pandey <51474798+DurgeshpdPandey@users.noreply.github.com>
2021-02-04 10:40:49 +0530

    Update RegistrationDelegatesImpl.java

--- RegistrationDelegatesImpl.java
+++ RegistrationDelegatesImpl.java
@@ -1,38 +1,40 @@
-package com.nacre.demo.DelegatesImpl;
+package com.DelegatesImpl;
 
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import com.nacre.demo.delegatesI.IRegistrationDelegates;
-import com.nacre.demo.dto.RegistrationDTO;
-import com.nacre.demo.serviceImpl.RegistrationServiceImpl;
-import com.nacre.demo.vo.RegistrationVO;
+import com.delegatesI.IRegistrationDelegates;
+import com.dto.RegistrationDTO;
+import com.serviceImpl.RegistrationServiceImpl;
+import com.vo.RegistrationVO;
 
 public class RegistrationDelegatesImpl implements IRegistrationDelegates{
 
 	@Override
 	public boolean parse(RegistrationVO registrationVO) {
+	//local variables	
 		String fname=null;
 		String lname=null;
 		String gender=null;		
-		//parsing logic
+	//parsing logic
 		int age=0;
 		double perc=0.0;
+	//date parsing	
 		DateFormat dateFormat;
 		Date utilDate;
 		java.sql.Date sqlDate = null;
 		RegistrationDTO registrationDTO;
 		RegistrationServiceImpl registrationServiceImpl;
 		
-		//getting values on variable
+	//getting values on variable
 		fname=registrationVO.getFname();
 		lname=registrationVO.getLname();
 		age=Integer.parseInt(registrationVO.getAge());
 		perc=Double.parseDouble(registrationVO.getPer());
 		gender=registrationVO.getGender();
-		//converting java date into sql date
+	//converting java date into sql date
 		dateFormat=new SimpleDateFormat("yyyy-MM-dd");
 		try {
 			utilDate=dateFormat.parse(registrationVO.getDob());
@@ -40,9 +42,9 @@ public class RegistrationDelegatesImpl implements IRegistrationDelegates{
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
-		//creating RegistrationDTO object
+	//creating RegistrationDTO object
 		registrationDTO=new RegistrationDTO();
-		//getting values on registrationDTO variables
+	//getting values on registrationDTO variables
 		registrationDTO.setFname(registrationVO.getFname());
 		registrationDTO.setLname(registrationVO.getLname());
 		registrationDTO.setGender(gender);
@@ -51,7 +53,7 @@ public class RegistrationDelegatesImpl implements IRegistrationDelegates{
 		registrationDTO.setDob(sqlDate);
 		registrationServiceImpl=new RegistrationServiceImpl();
 		
-		//returning register method of registrationServiceImpl
+	//returning register method of registrationServiceImpl
 		return registrationServiceImpl.register(registrationDTO);
 	}
 
